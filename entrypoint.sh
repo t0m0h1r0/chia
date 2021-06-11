137e780b7b3069b7bdf7627435cff1cab7a4de82
Tomohiro Kawaguchi <tomohiro@Tomohiro-no-MacBook-Pro.local>
2021-06-11 12:11:25 -0700

    improvement oof readability

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,7 +32,7 @@ elif [ ${mode} = "plotter" ];then
   work_dir=${tmp_dir}/`hostname`
   mkdir ${work_dir}
   rm -rf ${work_dir}/*
-  chia plots create -f ${farmer_key} -p ${pool_key} -t ${work_dir} -d ${plots_dir} -k 32 -n ${loop} -b 4500 -r ${thread}
+  chia plots create -f ${farmer_key} -p ${pool_key} -t ${work_dir} -d ${plots_dir} -n ${loop} -r ${thread} -k 32 -b 4500
   rm -rf ${work_dir}
 
 elif [ ${mode} = "plotter-fast" ];then
