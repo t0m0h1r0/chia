0fead70ccbfa53ef84f8a5c0d5a4492741b5b664
Tomohiro Kawaguchi <tomohiro@Tomohiro-no-MacBook-Pro.local>
2021-06-16 09:55:03 -0700

    revise dir info

--- setup_cluster.sh
+++ setup_cluster.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-#ssh root@192.168.10.40 "docker volume create chia_plots -o device=/srv/dev-disk-by-uuid-0c84cc82-b2fa-40f8-8d48-237032b6b48f/chia/plots -o type=none -o o=bind"
-#ssh root@192.168.10.50 "docker volume create chia_plots -o device=~/chia/plots -o type=none -o o=bind"
-#ssh admin@192.168.10.100 "/share/CACHEDEV1_DATA/.qpkg/container-station/bin/docker volume create chia_plots -o device=/share/chia/plots -o type=none -o o=bind"
-#ssh admin@192.168.10.101 "/share/CACHEDEV1_DATA/.qpkg/container-station/bin/docker volume create chia_plots -o device=/share/chia/plots -o type=none -o o=bind"
-#ssh root@192.168.10.102 "docker volume create chia_plots -o device=/VOL/chia/plots -o type=none -o o=bind"
+CHIA_PLOTS="chia_plots"
+DOCER_DIR="docker_dir"
+OPT="-o type=none -o o=bind"
 
-ssh root@192.168.10.40 "docker volume create docker_dir -o device=/var/lib/docker/ -o type=none -o o=bind"
-ssh root@192.168.10.50 "docker volume create docker_dir -o device=/var/lib/docker/ -o type=none -o o=bind"
-ssh admin@192.168.10.100 "/share/CACHEDEV1_DATA/.qpkg/container-station/bin/docker volume create docker_dir -o device=/share/CACHEDEV1_DATA/.qpkg/container-station/var/lib/docker/ -o type=none -o o=bind"
-ssh admin@192.168.10.101 "/share/CACHEDEV1_DATA/.qpkg/container-station/bin/docker volume create docker_dir -o device=/share/CACHEDEV1_DATA/.qpkg/container-station/var/lib/docker/ -o type=none -o o=bind"
-ssh root@192.168.10.102 "docker volume create docker_dir -o device=/apps/docker-cli-rnapp/ -o type=none -o o=bind"
+echo ssh root@192.168.10.40 "docker volume create ${CHIA_PLOTS} -o device=/srv/dev-disk-by-uuid-0c84cc82-b2fa-40f8-8d48-237032b6b48f/chia/plots/ ${OPT}"
+echo ssh root@192.168.10.50 "docker volume create ${CHIA_PLOTS} -o device=/root/chia/plots -o type=none -o o=bind"
+echo ssh admin@192.168.10.100 "/share/CACHEDEV1_DATA/.qpkg/container-station/bin/docker volume create ${CHIA_PLOTS} -o device=/share/chia/plots/ ${OPT}"
+echo ssh admin@192.168.10.101 "/share/CACHEDEV1_DATA/.qpkg/container-station/bin/docker volume create ${CHIA_PLOTS} -o device=/share/chia/plots/ ${OPT}"
+echo ssh root@192.168.10.102 "docker volume create ${CHIA_PLOTS} -o device=/VOL/chia/plots/ ${OPT}"
+
+echo ssh root@192.168.10.40 "docker volume create ${DOCER_DIR} -o device=/var/lib/docker/ ${OPT}"
+echo ssh root@192.168.10.50 "docker volume create ${DOCER_DIR} -o device=/var/lib/docker/ ${OPT}"
+echo ssh admin@192.168.10.100 "/share/CACHEDEV1_DATA/.qpkg/container-station/bin/docker volume create ${DOCER_DIR} -o device=/share/CACHEDEV1_DATA/.qpkg/container-station/var/lib/docker/ ${OPT}"
+echo ssh admin@192.168.10.101 "/share/CACHEDEV1_DATA/.qpkg/container-station/bin/docker volume create ${DOCER_DIR} -o device=/share/CACHEDEV1_DATA/.qpkg/container-station/var/lib/docker/ ${OPT}"
+echo ssh root@192.168.10.102 "docker volume create ${DOCER_DIR} -o device=/apps/docker-cli-rnapp/ ${OPT}"
